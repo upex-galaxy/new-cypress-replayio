6324ecd729a930bb1aa4594b8bcd419e735037ff
saiotest <elyermad@gmail.com>
2024-08-10 03:18:55 -0300

    init project

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig: defineCypressConfig } = require("cypress");
+const { plugin: replayPlugin } = require("@replayio/cypress");
+import 'dotenv/config';
+
+module.exports = defineCypressConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      const cyOn = wrapOn(on)
+      replayPlugin(cyOn, config, {
+        apiKey: process.env.REPLAY_API_KEY,
+        upload: true,
+      });
+      return config;
+    },
+  },
+});
